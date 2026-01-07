efeef130b9e8e42082c7fea751e958f9897d180b
kuyc <82702782+kuyc@users.noreply.github.com>
2026-01-07 13:52:16 +0300

    Update web.py

--- src/web.py
+++ src/web.py
@@ -1,13 +1,15 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import threading
 
-class H(BaseHTTPRequestHandler):
+class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200); self.end_headers()
+        self.send_response(200)
+        self.end_headers()
         self.wfile.write(b"OK")
 
 def start_web():
     threading.Thread(
-        target=HTTPServer(("0.0.0.0",8080),H).serve_forever,
+        target=HTTPServer(("0.0.0.0", 8080), Handler).serve_forever,
         daemon=True
     ).start()
+
