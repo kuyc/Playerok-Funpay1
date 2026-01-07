5dfd8a08ade9d5e6faf6e5365d54a7fbea720fc7
kuyc <82702782+kuyc@users.noreply.github.com>
2026-01-07 13:37:36 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,19 +5,19 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from config import BOT_TOKEN
 from bot.handlers import register_handlers
 from bot.scanner import start_scanner
+from web import start_web
 
 logging.basicConfig(level=logging.INFO)
-
 
 def main():
     bot = Bot(token=BOT_TOKEN, parse_mode="HTML")
     dp = Dispatcher(bot, storage=MemoryStorage())
 
     register_handlers(dp)
-    start_scanner(dp.bot)
+    start_scanner(bot)
+    start_web()
 
     executor.start_polling(dp, skip_updates=True)
-
 
 if __name__ == "__main__":
     main()
