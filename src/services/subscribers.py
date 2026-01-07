2dc4ce4675ef491df68017447b8b3dd3efd6ace3
kuyc <82702782+kuyc@users.noreply.github.com>
2026-01-07 13:55:34 +0300

    Update subscribers.py

--- src/services/subscribers.py
+++ src/services/subscribers.py
@@ -1,10 +1,4 @@
-SUBSCRIBERS = set()
-
-def add_user(user_id: int):
-    SUBSCRIBERS.add(user_id)
-
-def remove_user(user_id: int):
-    SUBSCRIBERS.discard(user_id)
-
-def get_all():
-    return list(SUBSCRIBERS)
+SUBSCRIBERS=set()
+def add_user(uid): SUBSCRIBERS.add(uid)
+def remove_user(uid): SUBSCRIBERS.discard(uid)
+def get_all(): return list(SUBSCRIBERS)
